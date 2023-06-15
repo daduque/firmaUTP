83f891d0716ef37f5f92798de93c24de9011a461
daanduqueutp <daanduque@utp.edu.co>
2023-06-15 16:44:24 -0500

    copy the code from html to js literal string

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,31 +12,38 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
         
         firma.innerHTML = `
-        <div  style="align-content: center; align-items: center; display: flex; flex-wrap: wrap; width: 550px;">
+        <div  style="display:inline-block; min-height:130px; vertical-align:middle; width:550px;">
                         
         <!-- imagen -->
-        <a  style="color:white; font-weight: bold; text-decoration: none;" href="https://www.utp.edu.co">
-            <img style="margin-right: 10px;" class="utp-azul" src="https://media2.utp.edu.co/imagenes/Logo-UTP-Azul.png" width="120" alt="">
+        <a  style="display: inline; color:white; font-weight: bold; text-decoration: none; vertical-align: middle;" href="https://www.utp.edu.co">
+            <img style="display:inline-block; 
+                margin-right: 10px; 
+                /* margin-top: 10px;  */
+                vertical-align: middle;"
+                class="utp-azul" src="https://media2.utp.edu.co/imagenes/logo-utp-azul-noborder.png" width="120" alt="Marca utp azul">
         </a>
         
         <!-- separador -->
 
-        <img src="https://ci5.googleusercontent.com/proxy/lvLIP8ep_ETbuzCBauiedvVf4W4EfCAam8aGqTXHQkdJ3Z_3825utYAY8ymjcHzyRopVe3-61qPWM5CMOtXCrwsBh4PZjzuLQZ61tcjx_ej033cQ86kmTA=s0-d-e1-ft#https://media.utp.edu.co/firma_digital/images/VicerrectoriasLinea.png"
+        <div style="display:inline-block">
+
+            <img src="https://ci5.googleusercontent.com/proxy/lvLIP8ep_ETbuzCBauiedvVf4W4EfCAam8aGqTXHQkdJ3Z_3825utYAY8ymjcHzyRopVe3-61qPWM5CMOtXCrwsBh4PZjzuLQZ61tcjx_ej033cQ86kmTA=s0-d-e1-ft#https://media.utp.edu.co/firma_digital/images/VicerrectoriasLinea.png"
             style="border:0px none;vertical-align:middle" class="CToWUd" data-bit="iit" width="2px"
-            height="100px">
+            height="110px">
+        </div>
 
         <!-- texto -->
-        <div>
-            <div style="padding-left:15px; color:black; display:table-cell; font-family:Helvetica;font-size:14px; height: 120px;line-height: 18px; vertical-align: middle ;width:380px">
+        <div style="display:inline-block;vertical-align:middle">
+            <div style="color:black; display:table-cell; font-family:Helvetica;font-size:14px; height: 120px;line-height: 18px; padding-left:15px; vertical-align: middle; width:380px">
                 <div style="font-weight: bold;"> ${ dataFirma.elements[0].value.toUpperCase() } </div>
                 <div style="font-family:Helvetica">
                     <div style="min-width:328px"> ${ dataFirma.elements[1].value.toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))) } </div>
-                    <div style="font-style:italic;font-weight:bold">${ dataFirma.elements[2].value }</div>
-                    <div>Ext:&nbsp;<span style="font-weight:700">${ dataFirma.elements[3].value }</span>&nbsp;/ Edificio Nº&nbsp;<span
-                            style="font-weight:700">${ dataFirma.elements[4].value }</span>&nbsp;/ Of:&nbsp;<span
-                            style="font-weight:700">${ dataFirma.elements[4].value } - ${ dataFirma.elements[5].value }</span></div>
+                    <div style="font-style:italic;font-weight:bold"> ${ dataFirma.elements[2].value } </div>
+                    <div>Ext:&nbsp;<span style="font-weight:700"> ${ dataFirma.elements[3].value } </span>&nbsp;/ Edificio Nº&nbsp;<span
+                            style="font-weight:700"> ${ dataFirma.elements[4].value } </span>&nbsp;/ Of:&nbsp;<span
+                            style="font-weight:700"> ${ dataFirma.elements[4].value } - ${ dataFirma.elements[5].value } </span></div>
                     <div>
-                        <a style="color:#003d6d" class="enlace-institucional" href="mailto:${ dataFirma.elements[6].value }" target="_blank">${ dataFirma.elements[6].value }</a>
+                        <a style="color:#003d6d" class="enlace-institucional" href="mailto:${ dataFirma.elements[6].value }" target="_blank"> admweb@utp.edu.co </a>
                     </div>
                 </div>
             </div>
@@ -45,64 +52,67 @@ document.addEventListener('DOMContentLoaded', (e) => {
     </div>
     <!-- barra inferior -->
     <div style="
-    align-content: center; 
-    align-items: center; 
-    color:white; 
     background-color: #003d6d;
-    display: flex;
+    color:white;
+    display: block;
     font-family:Helvetica; 
     font-size:14px; 
     height: 32px; 
-    justify-content: center; 
     padding-left:10px;
+    /* vertical-align: middle; */
     width: 550px;" 
     >
 
+        <div style="display: inline-block; height: 22px; margin-top: 5px; vertical-align: middle;">
 
-        <!-- Web -->
-        <a target="_blank" style="margin-top:6px; height:100%; color:white; margin-right: 10px; " href="https://www.utp.edu.co">
-            <img width="20" height="20" title="ir a la web" src="https://media2.utp.edu.co/iconos/sociales/WebB.png" alt="icono web" >
-        </a>
-
-
-    <!-- redes -->
-        <a target="_blank" style="margin-top:6px; height:100%; margin-right: 10px; " href="https://facebook.com/utpereira"> 
-            <img width="20" height="20" title="ir a facebook" src="https://media2.utp.edu.co/iconos/sociales/FacebookB.png" alt="icono facebook">
-        </a>
-        <a target="_blank" style="margin-top:6px; height:100%; margin-right: 10px;" href="https://twitter.com/utpereira">
-            <img width="20" height="20" title="ir a twitter" src="https://media2.utp.edu.co/iconos/sociales/TwitterB.png" alt="icono twitter">
-        </a>
-        <a target="_blank" style="margin-top:6px; height:100%; margin-right: 10px;" href="https://youtube.com/utpereira">
-            <img width="20" height="20" title="ir a youtube" src="https://media2.utp.edu.co/iconos/sociales/YoutubeB.png" alt="icono youtube">
-        </a>
-        <a target="_blank" style="margin-top:6px; height:100%; margin-right: 10px;" href="https://instagram.com/utpereira">
-            <img width="20" height="20" title="ir a instagram" src="https://media2.utp.edu.co/iconos/sociales/InstagramB.png" alt="icono instagram">
-        </a>
-        <a target="_blank" style="margin-top:6px; height:100%; margin-right: 40px;" href="https://www.linkedin.com/school/universidad-tecnol-gica-de-pereira/">
-            <img width="20" height="20" title="ir a linkedin" src="https://media2.utp.edu.co/iconos/sociales/LinkedInB.png" alt="icono linkedin">
-        </a>
-
-
-    <!-- teléfono -->
-    <img width="20" height="20" title="Teléfono" src="https://media2.utp.edu.co/iconos/sociales/telephone-fill.png" alt="icono teléfono" style="margin-top: 6px;"> 
-    <div style="height: 100%;">
-
-
-            <span style="display: block; margin-top: 6px; margin-left: 10px;">
-                +57 606 3137300
-            </span>
+            <!-- Web -->
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 10px; vertical-align: middle;" href="https://www.utp.edu.co">
+                <img width="20" height="20" title="ir a la web" src="https://media2.utp.edu.co/iconos/sociales/WebB.png" alt="icono web" >
+            </a>
             
-        </div>
-    <div style="height: 100%;">
-
-
-            <span style="display: block; font-weight: bold; margin-top: 6px; margin-left: 40px;">
-                @UTPereira
-            </span>
             
+            <!-- redes -->
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 10px; vertical-align: middle; " href="https://facebook.com/utpereira"> 
+                <img width="20" height="20" title="ir a facebook" src="https://media2.utp.edu.co/iconos/sociales/FacebookB.png" alt="icono facebook">
+            </a>
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 10px; vertical-align: middle;" href="https://twitter.com/utpereira">
+                <img width="20" height="20" title="ir a twitter" src="https://media2.utp.edu.co/iconos/sociales/TwitterB.png" alt="icono twitter">
+            </a>
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 10px; vertical-align: middle;" href="https://youtube.com/utpereira">
+                <img width="20" height="20" title="ir a youtube" src="https://media2.utp.edu.co/iconos/sociales/YoutubeB.png" alt="icono youtube">
+            </a>
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 10px; vertical-align: middle;" href="https://instagram.com/utpereira">
+                <img width="20" height="20" title="ir a instagram" src="https://media2.utp.edu.co/iconos/sociales/InstagramB.png" alt="icono instagram">
+            </a>
+            <a target="_blank" style="display:inline-block; color:white; height: 32px; margin-right: 40px; vertical-align: middle;" href="https://www.linkedin.com/school/universidad-tecnol-gica-de-pereira/">
+                <img width="20" height="20" title="ir a linkedin" src="https://media2.utp.edu.co/iconos/sociales/LinkedInB.png" alt="icono linkedin">
+            </a>
         </div>
+            
+            
+            <!-- teléfono -->
+            <div style="display: inline-block; height: 18px; margin-top: 5px; vertical-align: middle;">
+                <img width="16" title="Teléfono" src="https://media2.utp.edu.co/iconos/sociales/telephone-fill.png" alt="icono teléfono"> 
+                
+            </div>
+            <div style="display: inline-block; height: 18px; margin-top: 5px; vertical-align: middle;">
+                
+                <span style="display: inline-block; height: 32px; margin-left: 10px;">
+                    +57 606 3137300
+                </span>
+                
+            </div>
+            <div style="display: inline-block; height: 19px; margin-top: 5px; vertical-align: middle;">
+                
+                
+                <span style="display: inline-block; font-weight: bold; height: 32px; margin-left: 40px;">
+                    @UTPereira
+                </span>
+                
+            </div>
+        
     </div>
-
+        
     <div>
         <p style="color:black;font-family:Helvetica;font-size:14px; margin-top:2rem"> El contenido de este mensaje y sus anexos son únicamente para el uso del destinatario y pueden contener información  clasificada o reservada. Si usted no es el destinatario intencional, absténgase de cualquier uso, difusión, distribución o copia de esta comunicación.</p>
     </div>
